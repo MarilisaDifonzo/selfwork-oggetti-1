170752e1268f05b67ae553f7f0e334f509fbcec4
Marilisa Difonzo <marilisadifonzo17@gmail.com>
2026-09-01 17:46:40 +0200

    selfwork completato

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let person = {
     nome: 'Angela',
-    cognome: 'Aruanno'
+    cognome: 'Aruanno',
     eta: 24,
     presentarsi: function(){
         console.log(`ciao, sono ${this.nome} ${this.cognome} e ho ${this.eta} anni`);
